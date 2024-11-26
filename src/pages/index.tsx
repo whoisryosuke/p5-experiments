2531ce72caccaabbda8f58b580f9e0a82dc0ffea
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-11-26 12:01:37 -0800

    :sparkles: bluesky firehose basics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,60 +1,36 @@
-import Sidebar from "@/components/dom/Sidebar/Sidebar";
-import ClipTrackPreview from "@/components/screens/ClipScreen/ClipTrackPreview/ClipTrackPreview";
-import DebugInput from "@/features/input/DebugInput";
-import MusicSwitcher from "@/features/Music/MusicSwitcher";
-import OscillatorMusic from "@/features/Music/OscillatorMusic";
-import useStore from "@/helpers/store";
-import dynamic from "next/dynamic";
-import { useRouter } from "next/router";
-// import Shader from '@/components/canvas/ShaderExample/ShaderExample'
+import { readdirSync } from "fs";
+import Link from "next/link";
+import path from "path";
 
-// Prefer dynamic import for production builds
-// But if you have issues and need to debug in local development
-// comment these out and import above instead
-// https://github.com/pmndrs/react-three-next/issues/49
-const Shader = dynamic(
-  () => import("@/components/canvas/ShaderExample/ShaderExample"),
-  {
-    ssr: false,
-  }
-);
-const ClipScreen = dynamic(
-  () => import("@/components/screens/ClipScreen/ClipScreen"),
-  {
-    ssr: false,
-  }
-);
-
-// DOM elements here
-const DOM = () => {
-  return <></>;
-};
-
-// Canvas/R3F components here
-const R3F = () => {
-  // Example of using the router to change pages
-  // It can also be inside R3F component (see `two.tsx` and `Box.tsx`)
-  const { router } = useStore();
-  const handleOnClick = () => {
-    router.push("/two");
-  };
-
-  return <></>;
-};
-
-export default function Page() {
+export default function Page({ pages }) {
   return (
     <>
-      <DOM />
-      <R3F />
+      <div style={{ padding: "3rem" }}>
+        <h1>Experiments</h1>
+        <ul>
+          {pages.map((page) => (
+            <li key={page}>
+              <Link href={`/experiments/${page}`}>
+                {page.replace("-", " ")}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
 
 export async function getStaticProps() {
+  const experimentPath = path.join("./src/pages/experiments");
+  const experimentPages = readdirSync(experimentPath);
+  const pages = experimentPages.map((page) => page.replace(".tsx", ""));
+  console.log("experiment pages", pages);
+
   return {
     props: {
       title: "",
+      pages,
     },
   };
 }
--- src/site.config.ts
+++ src/site.config.ts
@@ -1,10 +1,10 @@
 const config = {
-  title: `Ryoturia`,
-  url: `https://github.com/whoisryosuke/ryoturia-web`,
+  title: `Ryo's P5 Sketchbook`,
+  url: `https://github.com/whoisryosuke/p5-experiments`,
   description:
     "This is a batteries included template for using React Three Fiber (R3F) with NextJS 🔋.",
   author: "Ryosuke Hana",
-  keywords: ["3d midi", "3d piano", "r3f midi", "r3f music"],
+  keywords: ["p5js", "generative art", "creative coding", "p5.js"],
   twitter: "@whoisryosuke",
 };
 
