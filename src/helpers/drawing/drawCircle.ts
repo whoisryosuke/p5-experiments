1ce703e9ea76607065bb68891cb79e4cb0d52c13
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-11-25 16:44:09 -0800

    :zap: add rotate to circle

--- src/helpers/drawing/drawCircle.ts
+++ src/helpers/drawing/drawCircle.ts
@@ -1,17 +1,22 @@
 import p5 from "p5";
+import { rotateXY } from "./rotateXY";
 
 export const drawCircle = (
   p: p5,
   radius: number,
   resolution: number,
   startX: number,
-  startY: number
+  startY: number,
+  rotateAngle: number = 0
 ) => {
   p.beginShape(p.POINTS);
   new Array(resolution).fill(0).forEach((_, index) => {
     const angle = (p.TWO_PI / resolution) * index;
-    const x = startX + p.cos(angle) * radius;
-    const y = startY + p.sin(angle) * radius;
+    const baseX = startX + p.cos(angle) * radius;
+    const baseY = startY + p.sin(angle) * radius;
+
+    const { x, y } = rotateXY(p, baseX, baseY, startX, startY, rotateAngle);
+
     p.vertex(x, y);
   });
   p.endShape();
