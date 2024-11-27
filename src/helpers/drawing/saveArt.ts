72bdc3d06240b3e59d0c5f44c46a0ffdd559bfcd
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-11-27 12:32:14 -0800

    :zap: allow for different key - conflicts with midi sometimes

--- src/helpers/drawing/saveArt.ts
+++ src/helpers/drawing/saveArt.ts
@@ -1,7 +1,7 @@
 import p5 from "p5";
 
-export const saveArt = (p: p5, filename: string) => {
-  if (p.key === "s") {
+export const saveArt = (p: p5, filename: string, saveKey: string = "s") => {
+  if (p.key === saveKey) {
     const date = new Date();
     const time = `${date.getFullYear()}${date.getMonth()}${date.getDay()}-${date.getMilliseconds()}`;
     p.saveCanvas(`${filename}-${time}`);
