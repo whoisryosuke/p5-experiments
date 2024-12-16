16615868eceb7ffe04e7b86e7949d50f1cbf39f4
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-12-16 15:56:26 -0800

    :construction: tonal method

--- src/features/MusicTeacher/MusicTeacher.tsx
+++ src/features/MusicTeacher/MusicTeacher.tsx
@@ -2,7 +2,8 @@ import { useInputStore } from "@/store/input";
 import React, { useEffect, useState } from "react";
 import { GeneratorConfig, TeoriaNote } from "./types";
 import teoria from "teoria";
-import { Chord } from "tonal";
+import { Chord, Note } from "tonal";
+import { CHORD_SYMBOLS, CHORD_TYPES } from "./data/chord-symbols";
 
 type Props = {};
 
@@ -17,30 +18,48 @@ const MusicTeacher = (props: Props) => {
 
   // Hydrate app with initial keys
   useEffect(() => {
+    // Teoria Method:
     // Create note
-    const fixedNote = rootNote.replace("#", "b");
-    const note = teoria.note(`${fixedNote}${octave}`);
-    let newNotes: TeoriaNote[] = [];
-    switch (config.type) {
-      case "scale":
-        newNotes = note.scale(config.scale).notes();
-        break;
-      case "chord":
-        newNotes = note.chord("sus2").notes();
-        break;
-    }
-    console.log("new notes", newNotes);
+    // const fixedNote = rootNote.replace("#", "b");
+    // const note = teoria.note(`${fixedNote}${octave}`);
+    // let newNotes: TeoriaNote[] = [];
+    // switch (config.type) {
+    //   case "scale":
+    //     newNotes = note.scale(config.scale).notes();
+    //     break;
+    //   case "chord":
+    //     newNotes = note.chord("sus2").notes();
+    //     break;
+    // }
+    // console.log("new notes", newNotes);
 
-    const notes = Chord.get("Cmaj7").notes;
-    console.log("tonal chord", notes);
+    // const newInput = newNotes.reduce((merge, note) => {
+    //   const inputKey = `${note.name().toLocaleUpperCase()}${note.octave()}`;
+    //   return {
+    //     ...merge,
+    //     [inputKey]: true,
+    //   };
+    // }, {});
 
-    const newInput = newNotes.reduce((merge, note) => {
-      const inputKey = `${note.name().toLocaleUpperCase()}${note.octave()}`;
+    // Tonal method
+    // const randomChord =
+    //   CHORD_SYMBOLS[Math.round(Math.random() * CHORD_SYMBOLS.length)];
+    // const notes = Chord.get(randomChord);
+
+    const randomType =
+      CHORD_TYPES[Math.round(Math.random() * CHORD_TYPES.length)];
+    const chord = Chord.getChord(randomType, "C", "B");
+    // const notes = Chord.notes("maj7", "C4");
+    console.log("tonal chord", chord.name, chord.notes);
+
+    const newInput = chord.notes.reduce((merge, note) => {
+      const inputKey = `${note}4`;
       return {
         ...merge,
         [inputKey]: true,
       };
     }, {});
+
     setMultiAppInput(newInput);
   }, []);
 
