586394c89a9f7007668fc27774ebbe1f36aa614e
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-01-09 13:57:10 -0800

    :construction: midi input + styling

--- src/experiments/MidiCalendarR1.tsx
+++ src/experiments/MidiCalendarR1.tsx
@@ -2,6 +2,7 @@ import P5Sketch from "@/components/P5Sketch";
 import { drawCircle } from "@/helpers/drawing/drawCircle";
 import { saveArt } from "@/helpers/drawing/saveArt";
 import { Position2D } from "@/helpers/types";
+import { useInputStore, UserInputMap } from "@/store/input";
 import { animate, AnimationSequence } from "motion";
 import p5 from "p5";
 import React from "react";
@@ -12,6 +13,7 @@ function daysInMonth(month, year) {
 }
 
 const DAYS_IN_A_WEEK = 7;
+const DAYS_IN_A_MONTH = 5;
 
 type DayParticle = {
   index: number;
@@ -28,21 +30,42 @@ const FILENAME = "MidiCalendarR1";
 
 const MidiCalendarR1 = (props: Props) => {
   const Sketch = (p: p5) => {
-    let y = 100;
+    let fontFamily = null;
+    let currentDate = new Date();
     let currentDayIndex = 0;
     let dayParticles: DayParticle[] = [];
-    p.setup = () => {
-      console.log("setup canvas");
-      p.createCanvas(window.innerWidth, window.innerHeight);
-      p.stroke(255); // Set line drawing color to white
-      p.frameRate(30);
+    let localInput: Partial<UserInputMap> = {
+      C4: false,
+      D4: false,
+      E4: false,
+      F4: false,
+      G4: false,
+      A4: false,
+      B4: false,
+      Cb4: false,
+      Db4: false,
+      Fb4: false,
+      Gb4: false,
+      Ab4: false,
+    };
 
-      // Hydrate in day particles
-      const currentDate = new Date();
+    const changeToNextMonth = () => {
+      currentDate = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() + 1,
+        1
+      );
+      currentDayIndex = 0;
+    };
+
+    const generateParticlesForMonth = () => {
+      dayParticles = [];
+
       const daysInCurrentMonth = daysInMonth(
         currentDate.getMonth(),
         currentDate.getFullYear()
       );
+
       new Array(daysInCurrentMonth).fill(0).map((_, dayNumber) => {
         const newParticle: DayParticle = {
           index: dayNumber,
@@ -57,60 +80,108 @@ const MidiCalendarR1 = (props: Props) => {
         dayParticles.push(newParticle);
       });
     };
+
+    const destroyParticle = () => {
+      const currentParticle = dayParticles[currentDayIndex];
+      currentParticle.destroy = true;
+      currentParticle.startTime = Date.now();
+
+      // Trigger animation
+      const sequence: AnimationSequence = [
+        [
+          currentParticle.animation,
+          {
+            x: 0,
+            y: -50,
+            opacity: 1,
+          },
+          { duration: 1 },
+        ],
+        [
+          currentParticle.animation,
+          {
+            x: 0,
+            y: -200,
+            opacity: 0,
+          },
+          { duration: 1 },
+        ],
+      ];
+      animate(sequence);
+
+      const nextIndex = currentDayIndex + 1;
+      if (nextIndex >= dayParticles.length) {
+        // Next month
+        setTimeout(() => {
+          console.log("Next month!");
+          changeToNextMonth();
+          generateParticlesForMonth();
+        }, 2000);
+      } else {
+        currentDayIndex = nextIndex;
+      }
+    };
+
+    p.setup = () => {
+      console.log("setup canvas");
+      p.createCanvas(window.innerWidth, window.innerHeight);
+      p.stroke(255); // Set line drawing color to white
+      p.frameRate(30);
+
+      // Load font
+      fontFamily = p.loadFont("../fonts/InterTight-SemiBold.ttf");
+
+      // Hydrate in day particles
+      generateParticlesForMonth();
+    };
     p.keyPressed = () => {
       saveArt(p, FILENAME);
-
-      if (p.key === "v") {
-        console.log("test destroy");
-        const currentParticle = dayParticles[currentDayIndex];
-        currentParticle.destroy = true;
-        currentParticle.startTime = Date.now();
-
-        // Trigger animation
-        const sequence: AnimationSequence = [
-          [
-            currentParticle.animation,
-            {
-              x: 0,
-              y: -50,
-              opacity: 1,
-            },
-            { duration: 1 },
-          ],
-          [
-            currentParticle.animation,
-            {
-              x: 0,
-              y: -200,
-              opacity: 0,
-            },
-            { duration: 1 },
-          ],
-        ];
-        animate(sequence);
-
-        const nextIndex = currentDayIndex + 1;
-        if (nextIndex > dayParticles.length) {
-          // Next month
-        } else {
-          currentDayIndex = nextIndex;
-        }
-      }
     };
     p.draw = () => {
       // console.log('drawing!!')
       p.background(p.color(BASE_COLORS["gray-9"])); // Set the background to black
 
-      p.strokeWeight(3);
+      const { input } = useInputStore.getState();
+
+      // Check if notes changed
+      const inputState = Object.entries(input).filter(([noteKey, noteState]) =>
+        noteKey.includes("4")
+      );
+      inputState.forEach(([noteKey, notePressed]) => {
+        // Key was pressed or released
+        if (localInput[noteKey] !== notePressed) {
+          // Spawn a note if pressed
+          if (notePressed) {
+            console.log("spawning note");
+            destroyParticle();
+          }
+          localInput[noteKey] = notePressed;
+        }
+      });
+
       const biggestEdge = p.width < p.height ? p.width : p.height;
 
       const screenPaddingX = biggestEdge / 4;
       const screenPaddingY = biggestEdge / 4;
 
       const boxSizeWidth = (p.width - screenPaddingX * 2) / DAYS_IN_A_WEEK;
-      const boxSizeHeight = 100;
-      const boxRadius = [16, 16, 16, 16];
+      const boxSizeHeight = (p.height - screenPaddingX * 2) / DAYS_IN_A_MONTH;
+      // const boxSizeHeight = 100;
+      const radius = 8;
+      const boxRadius = [radius, radius, radius, radius];
 
+      // Render framing
+      // Render the month name
+      const monthName = currentDate.toLocaleString("default", {
+        month: "long",
+      });
+      p.strokeWeight(0);
+      if (fontFamily !== null) p.textFont(fontFamily);
+      p.textSize(48);
+      p.textAlign(p.CENTER);
+      p.text(monthName, p.width / 2, screenPaddingX - 36);
+
+      // Render the days
       dayParticles.map((state, dayNumber) => {
         const weekNumber = Math.floor(dayNumber / 7);
         const baseX = dayNumber % 7;
@@ -123,7 +194,7 @@ const MidiCalendarR1 = (props: Props) => {
         const randomColor =
           THEME_COLORS[Math.floor(Math.random() * THEME_COLORS.length)];
         // p.fill(p.color(BASE_COLORS[`${randomColor}-5`]));
-        let fillColor = p.color(BASE_COLORS[`blue-5`]);
+        let fillColor = p.color(BASE_COLORS[`gray-7`]);
         if (state.destroy) {
           fillColor = p.color(BASE_COLORS[`red-5`]);
         }
@@ -133,10 +204,28 @@ const MidiCalendarR1 = (props: Props) => {
         // Stroke color
         const strokeColor = p.color(BASE_COLORS["gray-5"]);
         strokeColor.setAlpha(state.animation.opacity * 255);
+        p.strokeWeight(3);
         p.stroke(strokeColor);
 
         // Draw the box
-        p.rect(x, y, boxSizeWidth, boxSizeHeight, ...boxRadius);
+        const gridSpacingX = baseX == DAYS_IN_A_WEEK ? 0 : 16;
+        const gridSpacingY = weekNumber == DAYS_IN_A_MONTH ? 0 : 16;
+        p.rect(
+          x,
+          y,
+          boxSizeWidth - gridSpacingX,
+          boxSizeHeight - gridSpacingY,
+          ...boxRadius
+        );
+
+        // Draw date
+        const datePadding = 32;
+        let textColor = p.color(BASE_COLORS[`gray-2`]);
+        textColor.setAlpha(state.animation.opacity * 255);
+        p.fill(textColor);
+        p.strokeWeight(0);
+        p.textSize(16);
+        p.text(dayNumber, x + datePadding, y + datePadding);
       });
 
       // new Array(WEEKS_IN_A_MONTH).fill(0).map((_, baseY) => {
